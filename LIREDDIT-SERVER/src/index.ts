3ae7de8dc8db09d0db91ad431fbf677e45376673
henryhe1234 <henryhe1123@gmail.com>
2022-07-18 16:00:59 -0600

    cookie

--- LIREDDIT-SERVER/src/index.ts
+++ LIREDDIT-SERVER/src/index.ts
@@ -8,26 +8,54 @@ import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
 
+import * as redis from "redis";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import { MyContext } from "./types";
+
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
   const emFork = orm.em.fork(); // <-- create the fork
-  // const post = emFork.create(Post, {
-  //   // <-- use the fork instead of global `orm.em`
-  //   title: "my first post",
-  // } as RequiredEntityData<Post>);
-  // await emFork.persistAndFlush(post); // <-- use the fork instead of global
-
   const app = express();
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient({
+    legacyMode: true,
+  });
+  await redisClient.connect();
+
+  app.use(
+    session({
+      name: "qid",
+
+      store: new RedisStore({ client: redisClient, disableTouch: true }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, //10 years
+        httpOnly: true,
+        sameSite: "none", //csrf
+        secure: true, //cookie only works in https
+      },
+      saveUninitialized: false,
+      secret: "hfkalsjdfiowejskdlfj",
+      resave: false,
+    })
+  );
+  app.set("trust proxy", true);
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
-    context: () => ({ em: emFork }),
+
+    context: ({ req, res }): MyContext => ({ em: emFork, res, req }),
   });
+  const cors = {
+    credentials: true,
+    origin: "https://studio.apollographql.com",
+  };
   await apolloServer.start();
-  await apolloServer.applyMiddleware({ app });
+  await apolloServer.applyMiddleware({ app, cors });
 
   app.listen(4000, () => {
     console.log("server started on localhost:4000");
--- LIREDDIT-SERVER/src/types.ts
+++ LIREDDIT-SERVER/src/types.ts
@@ -1,5 +1,17 @@
 import { EntityManager, IDatabaseDriver, Connection } from "@mikro-orm/core";
+import { Request, Response } from "express";
+import { Session, SessionData } from "express-session";
+declare module "express-session" {
+  export interface Session {
+    userId: number;
+  }
+}
 
 export type MyContext = {
   em: EntityManager<IDatabaseDriver<Connection>>;
+  req: Request & {
+    session: Session & Partial<SessionData>;
+  };
+  // req: Request & { session: session.Session & { userId: number } };
+  res: Response;
 };
